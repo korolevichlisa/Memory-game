3833bd98ac3ef1d734f536c4f1d12a5a26cc9405
korolevichlisa <korolevich.liza@gmail.com>
2023-08-14 22:24:35 +0300

    innit

---
+++ app.js
@@ -0,0 +1,70 @@
+{
+  const temp = cardArray.map((item) => item);
+
+  for (const item of temp) {
+    cardArray.push(item);
+  }
+}
+
+cardArray.sort(() => 0.5 - Math.random());
+
+const displayGrid = document.querySelector("#grid");
+const resulDisplay = document.querySelector("#result");
+let cardsChoisen = [];
+let cardsChoisenIds = [];
+let cardsWon = [];
+
+function createBoard() {
+  for (let i = 0; i < cardArray.length; i++) {
+    const card = document.createElement("img");
+    card.setAttribute("src", "./images/blank.svg");
+    card.setAttribute("data-id", i);
+    card.addEventListener("click", flipCard);
+    displayGrid.appendChild(card);
+  }
+}
+
+createBoard();
+
+function checkMatch() {
+  const cards = document.querySelectorAll("img");
+  const optionOneId = cardsChoisenIds[0];
+  const optionTwoId = cardsChoisenIds[1];
+  if (optionOneId === optionTwoId) {
+    cards[optionOneId].setAttribute("src", "images/blank.svg");
+    cards[optionTwoId].setAttribute("src", "images/blank.svg");
+    alert("You have clicked the same image!");
+  } else if (cardsChoisen[0] === cardsChoisen[1]) {
+    alert("you are match!!!");
+    cards[optionOneId].classList.add("disable");
+    cards[optionTwoId].classList.add("disable");
+    cards[optionOneId].removeEventListener("click", flipCard);
+    cards[optionTwoId].removeEventListener("click", flipCard);
+
+    cardsWon.push(cardsChoisen);
+  } else {
+    cards[optionOneId].setAttribute("src", "images/blank.svg");
+    cards[optionTwoId].setAttribute("src", "images/blank.svg");
+    alert("sorry try again");
+  }
+
+  cardsChoisen = [];
+  cardsChoisenIds = [];
+  resulDisplay.textContent = cardsWon.length;
+
+  if (cardsWon.length == cardArray.length / 2) {
+    resulDisplay.textContent = "congratulations you found them all";
+  }
+}
+
+function flipCard() {
+  const cardId = this.getAttribute("data-id");
+
+  cardsChoisen.push(cardArray[cardId].name);
+  cardsChoisenIds.push(cardId);
+
+  this.setAttribute("src", cardArray[cardId].img);
+  if (cardsChoisen.length === 2) {
+    setTimeout(checkMatch, 500);
+  }
+}
---
+++ db.js
@@ -0,0 +1,26 @@
+const cardArray = [
+  {
+    name: "fries",
+    img: "images/fries.jpg",
+  },
+  {
+    name: "cheesburger",
+    img: "images/cheesburger.jpeg",
+  },
+  {
+    name: "hotdog",
+    img: "images/hotdog.jpeg",
+  },
+  {
+    name: "icecream",
+    img: "images/icecream.jpeg",
+  },
+  {
+    name: "milkshake",
+    img: "images/milkshake.jpg",
+  },
+  {
+    name: "pizzas",
+    img: "images/pizzas.jpg",
+  },
+];
